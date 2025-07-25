a7f96bed216329dd38eb89d2d0921e8e2b6a2467
Tenda <tendamadavha@gmail.com>
2025-07-25 17:37:35 +0200

    Updated contact page and minor changes with FAQ page

--- src/app/(main)/welcome/components/WelcomeText.tsx
+++ src/app/(main)/welcome/components/WelcomeText.tsx
@@ -1,9 +1,6 @@
 "use client";
 
 import React from "react";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import WelcomeBtn from "./WelcomeBtn";
 
 export default function WelcomeText() {
   return (
