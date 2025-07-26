c538ed5500efcb35f6106731c1c4b3c047c3c576
Tenda <tendamadavha@gmail.com>
2025-07-26 13:33:29 +0200

    fixed grid layout slowness issue

--- src/components/ui/layout-grid.tsx
+++ src/components/ui/layout-grid.tsx
@@ -11,19 +11,6 @@ type Card = {
 };
 
 export const LayoutGrid = ({ cards }: { cards: Card[] }) => {
-  const [selected, setSelected] = useState<Card | null>(null);
-  const [lastSelected, setLastSelected] = useState<Card | null>(null);
-
-  const handleClick = (card: Card) => {
-    setLastSelected(selected);
-    setSelected(card);
-  };
-
-  const handleOutsideClick = () => {
-    setLastSelected(selected);
-    setSelected(null);
-  };
-
   return (
     <div className="w-full h-full  grid grid-cols-1 md:grid-cols-3  max-w-7xl mx-auto gap-4 relative">
       {cards.map((card, i) => (
@@ -31,7 +18,7 @@ export const LayoutGrid = ({ cards }: { cards: Card[] }) => {
           <motion.div
             className={cn(
               card.className,
-              "relative overflow-hidden bg-white rounded-xl h-full w-full",
+              "relative overflow-hidden rounded-xl h-full w-full",
             )}
             layoutId={`card-${card.id}`}
           >
@@ -40,19 +27,10 @@ export const LayoutGrid = ({ cards }: { cards: Card[] }) => {
               <div className="absolute inset-0 z-20 flex flex-col justify-end p-4 bg-black/50">
                 {card.content}
               </div>
-              {selected?.id === card.id && <SelectedCard selected={selected} />}
             </>
           </motion.div>
         </div>
       ))}
-      <motion.div
-        onClick={handleOutsideClick}
-        className={cn(
-          "absolute h-full w-full left-0 top-0 bg-black opacity-0 z-10",
-          selected?.id ? "pointer-events-auto" : "pointer-events-none"
-        )}
-        animate={{ opacity: selected?.id ? 0.3 : 0 }}
-      />
     </div>
   );
 };
@@ -72,40 +50,4 @@ const ImageComponent = ({ card }: { card: Card }) => {
   );
 };
 
-const SelectedCard = ({ selected }: { selected: Card | null }) => {
-  return (
-    <div className="bg-transparent h-full w-full flex flex-col justify-end rounded-lg shadow-2xl relative z-[60]">
-      <motion.div
-        initial={{
-          opacity: 0,
-        }}
-        animate={{
-          opacity: 0.6,
-        }}
-        className="absolute inset-0 h-full w-full bg-black opacity-60 z-10"
-      />
-      <motion.div
-        layoutId={`content-${selected?.id}`}
-        initial={{
-          opacity: 0,
-          y: 100,
-        }}
-        animate={{
-          opacity: 1,
-          y: 0,
-        }}
-        exit={{
-          opacity: 0,
-          y: 100,
-        }}
-        transition={{
-          duration: 0.3,
-          ease: "easeInOut",
-        }}
-        className="relative px-8 pb-4 z-[70]"
-      >
-        {selected?.content}
-      </motion.div>
-    </div>
-  );
-};
+
